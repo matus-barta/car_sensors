af4fedf49eae1e0373e62d39980fb44aec5f1253
Matus Barta <matus.barta64@gmail.com>
2026-08-15 21:33:51 +0200

    add eslint ignore for shadcn ignore

--- www/eslint.config.js
+++ www/eslint.config.js
@@ -37,5 +37,10 @@ export default defineConfig(
 		// Override or add rule settings here, such as:
 		// 'svelte/button-has-type': 'error'
 		rules: {}
+	},
+	{
+		ignores: [
+			'src/lib/components/ui/**'
+		]
 	}
 );
