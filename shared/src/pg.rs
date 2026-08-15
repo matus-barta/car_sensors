5cd2ca2b395dd4d02b4f8dc67f4094696c0b640d
Matus Barta <matus.barta64@gmail.com>
2026-08-15 15:35:21 +0200

    fix applying migrations

--- shared/src/pg.rs
+++ shared/src/pg.rs
@@ -1,16 +1,22 @@
-pub async fn init_pg(db_url: String) -> sqlx::Pool<sqlx::Postgres> {
-    let conn = sqlx::postgres::PgPoolOptions::new()
+use sqlx::{Pool, Postgres, migrate::Migrator};
+
+static MIGRATOR: Migrator = sqlx::migrate!("../db/migrations");
+
+pub async fn init_pg(db_url: String) -> Pool<Postgres> {
+    let pool = sqlx::postgres::PgPoolOptions::new()
         .max_connections(20)
         .connect(&db_url)
         .await
-        .expect("Unable to connect to DB");
+        .expect("Unable to connect to database");
 
     tracing::info!("Connected to Postgres");
 
-    sqlx::migrate!("../db/migrations")
-        .run(&conn)
+    MIGRATOR
+        .run(&pool)
         .await
-        .expect("Migration failed!");
+        .expect("Unable to apply database migrations");
 
-    return conn;
+    tracing::info!("Database migrations are up to date");
+
+    pool
 }
